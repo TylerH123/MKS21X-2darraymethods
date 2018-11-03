40618385d1386abe87afaf85a4dbf99075db31c0
Tyler Huang <thuang00@stuy.edu>
2018-11-02 22:37:26 -0400

    a

---
+++ ArrayMethods.java
@@ -0,0 +1,18 @@
+public class ArrayMethods{
+
+  public static int rowSum(int[][] ary, int x){
+    int sum = 0;
+    for (int i = 0; i < ary[x].length; i++){
+      sum += ary[x][i];
+    }
+    return sum;
+  }
+  public static int columnSum(int[][] ary, int y){
+    int sum = 0;
+    for (int i = 0; i < ary.length; i++){
+      sum += ary[i][y];
+    }
+    return sum;
+  }
+  
+}
